59ad334a3105c14916b60d06b50e8a7fb03e5b75
Jay Rathi <jay.rathi@accordion.com>
2026-08-12 07:02:00 +0000

    external table tables created using macro

---
+++ macros/create_external_table.sql
@@ -0,0 +1,27 @@
+{% macro create_external_table(
+    table_name,
+    folder_name,
+    database_name='CT_JAY_RATHI_DB',
+    schema_name='EXTERNAL',
+    stage_name='CT_JAY_RATHI_DB.BRONZE.BRONZE_STAGE',
+    file_name='Capstone_Project_Data',
+    file_format='CT_JAY_RATHI_DB.BRONZE.JSON_FORMAT'
+) %}
+
+    {% set sql %}
+
+    CREATE OR REPLACE EXTERNAL TABLE {{ database_name }}.{{ schema_name }}.{{ table_name }}
+    (
+    RAW_DATA VARIANT AS (VALUE)
+    )
+    LOCATION = @{{ stage_name }}/{{file_name}}/{{ folder_name }}/
+    FILE_FORMAT = {{ file_format }}
+    AUTO_REFRESH = FALSE;
+
+    {% endset %}
+
+    {{ log(sql, info=True) }}
+
+    {% do run_query(sql) %}
+
+{% endmacro %}
